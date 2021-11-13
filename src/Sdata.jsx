539684aab868da6496df7ec0673652381725e213
Manish <manishgoyal6681@gmail.com>
2021-11-14 01:21:39 +0530

    added Crop page

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,39 +1,39 @@
 const Sdata = [
-    {
-        imgsrc:"fd",
-        title:" N",
-        sname: "uu", 
-        link: "cv",
-    },
-    
-    {
-       imgsrc:"fds" ,
-       title:"df",
-       sname: "b", 
-       link: "c",
+   {
+       imgsrc:"https://c1.wallpaperflare.com/preview/843/981/213/pearl-millet-pennisetum-glaucum-bajra-crop-spike-cereal.jpg",
+       title:"BAJRA",
+       sname: "5000 KG available", 
+       link: "",
+   },
+   
+   {
+      imgsrc:"https://images4.alphacoders.com/644/thumb-1920-644152.jpg" ,
+      title:"Wheat",
+      sname: "15000KG Wheat Available", 
+      link: "",
+   },
+
+   {
+      imgsrc:"https://images8.alphacoders.com/420/thumb-1920-420163.jpg" ,     
+      title: "Potato",
+      sname: "Best Quality 22000KG Available", 
+      link: "",
+   },
+   
+   {
+       imgsrc:"https://images8.alphacoders.com/399/thumb-1920-399003.jpg",
+       title: "Tomato",
+       sname: "Best Quality 12000KG Available", 
+       link: "",
     },
 
     {
-       imgsrc:"fsds" ,     
-       title: "j",
-       sname: "h", 
-       link: "fc",
+       imgsrc:"https://images2.alphacoders.com/832/8322.jpg"   ,     
+       title: "Corn",
+       sname: "Best Quality 16000KG Available", 
+       link: "",
     },
-    
-    {
-        imgsrc:"fds",
-        title: " j",
-        sname: "h", 
-        link: "f",
-     },
-
-     {
-        imgsrc:"f"   ,     
-        title: "Kisan Andolan",
-        sname: "Extra Curricular", 
-        link: "cdiu",
-     },
-    
+   
 ]
 
 export default Sdata;
