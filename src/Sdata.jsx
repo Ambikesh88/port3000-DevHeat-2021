eb2f86a129b6ebdd324e588c90dcc8b78b25d1db
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2021-11-13 12:00:52 +0530

    cards added

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata = [
+    {
+        imgsrc:"fd",
+        title:" N",
+        sname: "uu", 
+        link: "cv",
+    },
+    
+    {
+       imgsrc:"fds" ,
+       title:"df",
+       sname: "b", 
+       link: "c",
+    },
+
+    {
+       imgsrc:"fsds" ,     
+       title: "j",
+       sname: "h", 
+       link: "fc",
+    },
+    
+    {
+        imgsrc:"fds",
+        title: " j",
+        sname: "h", 
+        link: "f",
+     },
+
+     {
+        imgsrc:"f"   ,     
+        title: "Kisan Andolan",
+        sname: "Extra Curricular", 
+        link: "cdiu",
+     },
+    
+]
+
+export default Sdata;
