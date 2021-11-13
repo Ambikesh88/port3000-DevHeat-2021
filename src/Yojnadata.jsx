2bbc394539ddaeda0bc91487e7468092663ac8ce
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2021-11-13 23:15:41 +0530

    Yojna added

---
+++ src/Yojnadata.jsx
@@ -0,0 +1,45 @@
+const Yojnadata = [
+    {
+        imgsrc: "https://www.jagranimages.com/images/newimg/06082021/06_08_2021-aggriculture_21900667.jpg",
+        title:" Pradhan Mantri Krishi Sinchai Yojana (PMKSY)",
+        sname: "Har Khet ko Pani 'Prime Minister Krishi Sinchayee Yojana' Government of India is committed to accord high priority to water conservation and its management.",
+        link: "https://pmksy.gov.in/",
+    },
+    
+    {
+       imgsrc:"https://wallpapercave.com/wp/wp3708740.jpg" ,
+       title:"Paramparagat Krishi Vikas Yojana (PKVY) ",
+       sname: "The Paramparagat Krishi Vikas Yojana (PKVY), an initiative to promote organic farming in the country, was launched by the NDA government in 2015. ", 
+       link: "https://pgsindia-ncof.gov.in/PKVY/index.aspx",
+    },
+
+    {
+       imgsrc:"https://wallpapercave.com/wp/wp3708739.jpg" ,     
+       title: "Pradhan Mantri Fasal Bima Yojana (PMFBY)",
+       sname: "Pradhan Mantri Fasal Bima Yojana (PMFBY) is the government sponsored crop insurance scheme that integrates multiple stakeholders on a single platform.   ", 
+       link: "https://pmfby.gov.in/",
+    },
+    
+    {
+        imgsrc:" https://wallpapercave.com/wp/wp2024276.jpg ",
+        title: " Gramin Bhandaran Yojna ",
+        sname: "The objectives of this schemes are 1-Create scientific storage capacity with allied facilities in rural areas. 2- To meet the requirements of farmers for storing farm produce, processed farm produce and agricultural inputs Promotion of grading, standardization and quality control of agricultural produce to improve their marketability, promotion.", 
+        link: "https://pmmodiyojana.in/warehouse-subsidy-scheme/",
+     },
+
+     {
+        imgsrc:"https://wallpapercave.com/wp/wp3320041.jpg"   ,     
+        title: "Livestock insurance Scheme ",
+        sname: "This scheme aims to provide protection mechanism to the farmers and cattle rearers against any eventual loss of their animals due to death and to demonstrate the benefit of the insurance of livestock to the people and popularize it with the ultimate goal of attaining qualitative improvement in livestock and their products.", 
+        link: "https://dahd.nic.in/related-links/livestock-insurance",
+     },
+     {
+        imgsrc:"https://wallpapercave.com/wp/wp3708758.jpg"   ,     
+        title: "Micro Irrigation Fund (MIF)",
+        sname: "The government approved a dedicated Rs5,000 crore fund to bring more land area under micro-irrigation as part of its objective to boost agriculture production and farmers income.   The fund has been set up under NABARD, which will provide this amount to states on concessional rate of interest to promote micro-irrigation, which currently has a coverage of only 10 million hectares as against the potential of 70 million hectares.", 
+        link: "https://www.nabard.org/content1.aspx?id=1720&catid=8&mid=8",
+     },
+    
+]
+
+export default Yojnadata;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import "./index.css"; import Sdata from "./Sdata";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Yojna from "./Yojna";
 
+import Yojnadata from './Yojnadata';
 
 
 //used the props for using cards and other props
@@ -55,7 +57,43 @@ ReactDOM.render(
   sname={Sdata[4].sname}
   link={Sdata[4].link}/> 
  
+{/* //for yojna */}
+<h1 className="heading_style">Yojnas</h1>
 
+  <Card 
+  imgsrc={Yojnadata[0].imgsrc} 
+  title={Yojnadata[0].title}
+  sname={Yojnadata[0].sname}
+  link={Yojnadata[0].link}/>
+
+  <Card
+ imgsrc={Yojnadata[1].imgsrc} 
+ title={Yojnadata[1].title}
+ sname={Yojnadata[1].sname}
+ link={Yojnadata[1].link}/>
+
+  <Card
+  imgsrc={Yojnadata[2].imgsrc} 
+  title={Yojnadata[2].title}
+  sname={Yojnadata[2].sname}
+  link={Yojnadata[2].link}/>
+
+<Card
+  imgsrc={Yojnadata[3].imgsrc} 
+  title={Yojnadata[3].title}
+  sname={Yojnadata[3].sname}
+  link={Yojnadata[3].link}/>
+
+<Card
+  imgsrc={Yojnadata[4].imgsrc}  
+  title={Yojnadata[4].title}
+  sname={Yojnadata[4].sname}
+  link={Yojnadata[4].link}/> 
+  <Card
+  imgsrc={Yojnadata[5].imgsrc}  
+  title={Yojnadata[5].title}
+  sname={Yojnadata[5].sname}
+  link={Yojnadata[5].link}/> 
 
  </>,
    document.getElementById('root')
