8bc012231073a5e6ad69e8ae7f5c0f0b9ce2fd95
Manish <manishgoyal6681@gmail.com>
2021-11-18 09:09:54 +0530

    improved the index js using map function

--- src/index.js
+++ src/index.js
@@ -24,117 +24,46 @@ ReactDOM.render(
     </React.StrictMode>
   </BrowserRouter>,
   <h1 className="heading_style">List of Crops with Market Price</h1>
+  
+  {/* For the Crops */}
 
-  <Card 
-  imgsrc={Sdata[0].imgsrc} 
-  title={Sdata[0].title}
-  sname={Sdata[0].sname}
-  link={Sdata[0].link}/>
-
-  <Card
- imgsrc={Sdata[1].imgsrc} 
- title={Sdata[1].title}
- sname={Sdata[1].sname}
- link={Sdata[1].link}/>
-
-  <Card
-  imgsrc={Sdata[2].imgsrc} 
-  title={Sdata[2].title}
-  sname={Sdata[2].sname}
-  link={Sdata[2].link}/>
-
-<Card
-  imgsrc={Sdata[3].imgsrc} 
-  title={Sdata[3].title}
-  sname={Sdata[3].sname}
-  link={Sdata[3].link}/>
-
-<Card
-  imgsrc={Sdata[4].imgsrc}  
-  title={Sdata[4].title}
-  sname={Sdata[4].sname}
-  link={Sdata[4].link}/> 
-  <Card
-  imgsrc={Sdata[5].imgsrc}  
-  title={Sdata[5].title}
-  sname={Sdata[5].sname}
-  link={Sdata[5].link}/> 
+  {Sdata.map((val) => {
+    return (
+    <Card
+    imgsrc={val.imgsrc} 
+    title={val.title}
+    sname={val.sname}
+    link={val.link}
+    />
+    );
+  })}    
  
 {/* //for yojna */}
 <h1 className="heading_style">Yojnas</h1>
-
+{Yojnadata.map((val) => {
+  return(
   <Yojna 
-  imgsrc={Yojnadata[0].imgsrc} 
-  title={Yojnadata[0].title}
-  sname={Yojnadata[0].sname}
-  link={Yojnadata[0].link}/>
-
-  <Yojna
- imgsrc={Yojnadata[1].imgsrc} 
- title={Yojnadata[1].title}
- sname={Yojnadata[1].sname}
- link={Yojnadata[1].link}/>
-
-  <Yojna
-  imgsrc={Yojnadata[2].imgsrc} 
-  title={Yojnadata[2].title}
-  sname={Yojnadata[2].sname}
-  link={Yojnadata[2].link}/>
-
-<Yojna
-  imgsrc={Yojnadata[3].imgsrc} 
-  title={Yojnadata[3].title}
-  sname={Yojnadata[3].sname}
-  link={Yojnadata[3].link}/>
-
-<Yojna
-  imgsrc={Yojnadata[4].imgsrc}  
-  title={Yojnadata[4].title}
-  sname={Yojnadata[4].sname}
-  link={Yojnadata[4].link}/> 
-  <Yojna
-  imgsrc={Yojnadata[5].imgsrc}  
-  title={Yojnadata[5].title}
-  sname={Yojnadata[5].sname}
-  link={Yojnadata[5].link}/> 
+  imgsrc={val.imgsrc} 
+  title={val.title}
+  sname={val.sname}
+  link={val.link}
+  />
+  );
+})}  
 
 {/* for pest */}
 <h1 className="heading_style">Pest</h1>
 
+{Pestdata.map((val) => {
+  return(
   <Pest 
-  imgsrc={Pestdata[0].imgsrc} 
-  title={Pestdata[0].title}
-  sname={Pestdata[0].sname}
-  link={Pestdata[0].link}/>
-
-  <Pest
- imgsrc={Pestdata[1].imgsrc} 
- title={Pestdata[1].title}
- sname={Pestdata[1].sname}
- link={Pestdata[1].link}/>
-
-  <Pest
-  imgsrc={Pestdata[2].imgsrc} 
-  title={Pestdata[2].title}
-  sname={Pestdata[2].sname}
-  link={Pestdata[2].link}/>
-
-<Pest
-  imgsrc={Pestdata[3].imgsrc} 
-  title={Pestdata[3].title}
-  sname={Pestdata[3].sname}
-  link={Pestdata[3].link}/>
-
-<Pest
-  imgsrc={Pestdata[4].imgsrc}  
-  title={Pestdata[4].title}
-  sname={Pestdata[4].sname}
-  link={Pestdata[4].link}/> 
-  <Pest
-  imgsrc={Pestdata[5].imgsrc}  
-  title={Pestdata[5].title}
-  sname={Pestdata[5].sname}
-  link={Pestdata[5].link}/> 
+  imgsrc={val.imgsrc} 
+  title={val.title}
+  sname={val.sname}
+  link={val.link}
+  />
+  );
+})}
 
 
  </>,
